e8995448cd2043159b0c462cc3b29f6c7c5c8910
Tuyen Dang <tuyendq@gmail.com>
2023-11-03 16:17:13 +0700

    Complete Ch1 - Review Define Class

--- Start/Ch1/DefineClasses/Program.cs
+++ Start/Ch1/DefineClasses/Program.cs
@@ -2,6 +2,11 @@
 // Defining and instantiating classes
 
 // TODO: Create some new Rectangle objects with dimensions
-
+Rectangle rect1 = new Rectangle(3, 5);
+Rectangle rect2 = new Rectangle(40);
+System.Console.WriteLine(rect1.GetArea());
+System.Console.WriteLine(rect2.GetArea());
 
 // TODO: Change the values of width and height
+rect2.height = 50;
+System.Console.WriteLine(rect2.GetArea());
--- Start/Ch1/DefineClasses/shapes.cs
+++ Start/Ch1/DefineClasses/shapes.cs
@@ -2,15 +2,29 @@
 // Defining and instantiating classes
 
 // TODO: Define a class using the "class" keyword
+using System.Reflection.Metadata.Ecma335;
+
+public class Rectangle {
 
     // TODO: The constructor accepts parameters used to create the object
-
+    public Rectangle(int w, int h) {
+        width = w;
+        height = h;
+    }
 
     // TODO: For convenience, we can have a constructor that takes one value
     // for squares that have the same side size
-
+    public Rectangle(int side) {
+        width = height = side;
+    }
 
     // TODO: Classes can define methods that return values
-
+    public int GetArea() { 
+        return width * height;
+     }
 
     // TODO: member variables hold data
+    public int width;
+    public int height;
+
+}
